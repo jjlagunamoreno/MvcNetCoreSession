e2bc99ad8d6daa51ff6134c78d3a056efc495e34
jjlagunamoreno <jjlagunamoreno@gmail.com>
2025-02-18 10:04:40 +0100

    Add project files.

---
+++ MvcNetCoreSession/Controllers/EjemploSessionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcNetCoreSession.Controllers
+{
+    public class EjemploSessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SessionSimple(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    //GUARDAMOS DATOS EN SESSION
+                    HttpContext.Session.SetString("nombre", "Programeitor");
+                    HttpContext.Session.SetString("hora",
+                        DateTime.Now.ToLongTimeString());
+                    ViewData["MENSAJE"] = "Datos almacenados en Session";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    //RECUPERAMOS LOS DATOS DE SESSION
+                    ViewData["USUARIO"] = 
+                        HttpContext.Session.GetString("nombre");
+                    ViewData["HORA"] =
+                        HttpContext.Session.GetString("hora");
+                }
+            }
+            return View();
+        }
+    }
+}
